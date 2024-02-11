6ca75fcc58bbe142bce453a89b490abdcaf0fac9
Ellie <ellie@ellieis.me>
2024-02-11 16:45:45 -0300

    Update to 1.20.4 (#53)
    Co-authored-by: ellieisjelly <ellie@caiomgt.com>

--- src/main/java/us/potatoboy/skywars/mixin/ExplosionMixin.java
+++ src/main/java/us/potatoboy/skywars/mixin/ExplosionMixin.java
@@ -1,30 +1,26 @@
 package us.potatoboy.skywars.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.util.ActionResult;
-import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.explosion.ExplosionBehavior;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import us.potatoboy.skywars.SkyWars;
 import xyz.nucleoid.plasmid.game.manager.GameSpaceManager;
 
-@Mixin(Explosion.class)
+@Mixin(ExplosionBehavior.class)
 public abstract class ExplosionMixin {
-    @Final
-    @Shadow
-    private World world;
 
-    @ModifyConstant(method = "collectBlocksAndDamageEntities", constant = @Constant(doubleValue = 7.0))
-    private double reduceDamage(double original) {
-        var gameSpace = GameSpaceManager.get().byWorld(this.world);
+
+    @ModifyConstant(method = "calculateDamage", constant = @Constant(doubleValue = 7.0))
+    private double reduceDamage(double original, Explosion explosion, Entity entity) {
+        var gameSpace = GameSpaceManager.get().byWorld(entity.getWorld());
 
         if (gameSpace != null && gameSpace.getBehavior().testRule(SkyWars.REDUCED_EXPLOSION_DAMAGE) == ActionResult.SUCCESS) {
             return 4.0D;
         }
-
         return original;
     }
 }
